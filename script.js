f6ee6cbed9ee8b7c688c9b9234230bc20b65faff
vikasairsharma7071 <57059631+vikasairsharma7071@users.noreply.github.com>
2023-01-03 22:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let str = document.getElementById("evaluatedText");
+
+let mess = document.getElementById("letterCount");
+
+function check(){
+    let val = str.value;
+    let valLength = val.length+1;
+    mess.innerHTML = valLength; 
+}
+
+str.addEventListener("keypress",check);
